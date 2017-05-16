7aff3dacbfd408756ef6850b181fedc88700fd83
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-05-16 09:09:16 +0200

    ok

--- src/cmd/run.c
+++ src/cmd/run.c
@@ -5,21 +5,25 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Sun May 14 15:47:12 2017 arnaud.alies
-** Last update Mon May 15 18:24:44 2017 arnaud.alies
+** Last update Tue May 16 09:09:02 2017 arnaud.alies
 */
 
 #include <string.h>
 #include "server.h"
 
-t_cmd cmds[] = {
+static t_cmd g_unlogged_cmds[] = {
   DEF_CMD(quit),
   {NULL, NULL}
 };
 
-int	cmd_run(t_ftp *ftp, char *cmd)
+static t_cmd g_logged_cmds[] = {
+  {NULL, NULL}
+};
+
+static int	cmd_run_cmds(t_ftp *ftp, char *cmd, t_cmd *cmds, bool *run)
 {
-  int	len;
-  int	x;
+  int		len;
+  int		x;
 
   len = strlen(cmd);
   if (len <= 0)
@@ -29,9 +33,29 @@ int	cmd_run(t_ftp *ftp, char *cmd)
     {
       if (strncasecmp(cmds[x].str, cmd, len) == 0)
 	{
+	  *run = true;
 	  return (cmds[x].func(ftp, cmd));
 	}
       x += 1;
+    }
+  return (0);
+}
+
+int	cmd_run(t_ftp *ftp, char *str)
+{
+  bool	run;
+
+  run = false;
+  if (cmd_run_cmds(ftp, str, g_unlogged_cmds, &run))
+    return (1);
+  if (ftp->logged && run == false)
+    {
+      if (cmd_run_cmds(ftp, str, g_logged_cmds, &run))
+	return (1);
+    }
+  if (run == false)
+    {
+      //unknown cmd
     }
   return (0);
 }
