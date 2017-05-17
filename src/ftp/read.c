f4779a12b6833784fbdb257daf028376826b2daa
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-05-17 09:45:13 +0200

    ok

--- src/ftp/read.c
+++ src/ftp/read.c
@@ -5,7 +5,7 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Tue May 16 14:37:30 2017 arnaud.alies
-** Last update Tue May 16 18:58:33 2017 arnaud.alies
+** Last update Wed May 17 09:44:06 2017 arnaud.alies
 */
 
 #include <stdio.h>
@@ -18,7 +18,8 @@ int     ftp_read(t_ftp *ftp, char *buff, size_t size)
 
   if ((r = get_next_crlf(ftp->client->fd)) == NULL)
     return (1);
-  strncpy(buff, r, size);
+  memset(buff, 0, size);
+  strncpy(buff, r, size - 1);
   free(r);
   return (0);
 }
