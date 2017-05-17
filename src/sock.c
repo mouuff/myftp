550f0cc727d79a2325855ca137d4f5d2822d2ba9
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-05-17 13:32:51 +0200

    ok

---
+++ src/sock.c
@@ -0,0 +1,11 @@
+/*
+** sock.c for hue in /home/arnaud.alies/rendu/PSU_2016_myftp
+** 
+** Made by arnaud.alies
+** Login   <arnaud.alies@epitech.eu>
+** 
+** Started on  Wed May 17 11:56:13 2017 arnaud.alies
+** Last update Wed May 17 11:56:32 2017 arnaud.alies
+*/
+
+#include "server.h"
