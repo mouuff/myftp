82ea870254da52a84bd6737f320e07f0ad21ac8d
Arnaud Alies <arnaud.alies@arnaud.alies>
2017-05-18 10:33:32 +0200

    ok

--- src/cmd/list.c
+++ src/cmd/list.c
@@ -5,10 +5,36 @@
 ** Login   <arnaud.alies@epitech.eu>
 ** 
 ** Started on  Wed May 17 23:30:47 2017 arnaud.alies
-** Last update Thu May 18 10:17:59 2017 arnaud.alies
+** Last update Thu May 18 10:32:21 2017 arnaud.alies
 */
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include "server.h"
+
+static int	send_listing(int fd, char *dir)
+{
+  DIR		*dp;
+  struct dirent	*ep;
+
+  if (dir == NULL)
+    dp = opendir("./");
+  else
+    dp = opendir(dir);
+  if (dp == NULL)
+    return (1);
+  while ((ep = readdir(dp)))
+    {
+      if (sock_send(fd, ep->d_name))
+	{
+	  closedir(dp);
+	  return (0);
+	}
+    }
+  closedir (dp);
+  return (0);
+}
 
 int	cmd_list(t_ftp *ftp, t_args *args)
 {
@@ -23,7 +49,11 @@ int	cmd_list(t_ftp *ftp, t_args *args)
     }
   else
     {
-      write(fd, "hue\r\n", 5);
+      if (args->ac > 1)
+	send_listing(fd, (args->av)[1]);
+      else
+	send_listing(fd, NULL);
+      clean_close_fd(fd);
       return (ftp_send(ftp, FTP_CLOSING_DATA, "Closing data connection."));
     }
   return (0);
